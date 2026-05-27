4db9092b42e2e4deac124bd6414da8b47f357a22
Ryan-Nening <neningryan10@gmail.com>
2026-05-27 15:15:42 +0800

    I added interactive buttons para sa gas at preno to make the program maangassssss.

--- car_blueprint.py
+++ car_blueprint.py
@@ -31,4 +31,11 @@ class CarDashboard:
         tk.Label(self.main_window, text = f"Vehicle: {self.my_car.get_details()}", background = "#0b0c10", foreground = "#66fcf1", font = ("Helvetica", 14, "bold")).pack(pady = (20, 10))
         self.speed_label = tk.Label(self.main_window, text = str(self.my_car.get_speed()), font = ("Courier", 50, "bold"), background = "#0b0c10", foreground = "#45a29e")
         self.speed_label.pack(pady = 10)
+
+        self.button_frame = tk.Frame(self.main_window, background = "#060c10")
+        self.button_frame.pack(pady = 10)
+
+        tk.Button(self.btn_frame, text="Accelerate (+5)", bg="#2c3e50", fg="white", width=12, command=self.press_accelerate).grid(row=0, column=0, padx=10)
+        tk.Button(self.btn_frame, text="Brake (-5)", bg="#c0392b", fg="white", width=12, command=self.press_brake).grid(row=0, column=1, padx=10)
+
         
